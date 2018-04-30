f9283d3a0c681abca2e30df2a10764c9e84641e5
Danying Yang <15757124551@163.com>
2018-04-30 14:35:35 +0800

    页面中单独js

--- webpack/demo/webpack.config.js
+++ webpack/demo/webpack.config.js
@@ -9,28 +9,29 @@ module.exports = {
     },
     output: {
         path: './dist',
-        filename: 'js/[name]-[chunkhash].js'
+        filename: 'js/[name]-[chunkhash].js',
+        publicPath: 'http://www.cdn.com/'
     },
     plugins: [
         new htmlWebpackPlugin({
             filename: 'a.html',
             template: 'index.html',
             title: 'this is a.html',
-            inject: 'body',
+            inject: false,
             excludeChunks: ['b', 'c']
         }),
         new htmlWebpackPlugin({
             filename: 'b.html',
             template: 'index.html',
             title: 'this is b.html',
-            inject: 'body',
+            inject: false,
             chunks: ['main', 'b']
         }),
         new htmlWebpackPlugin({
             filename: 'c.html',
             template: 'index.html',
             title: 'this is c.html',
-            inject: 'body',
+            inject: false,
             chunks: ['main', 'c']
         })
     ]
